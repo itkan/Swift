da8d8da1cf8704052f096fe203700c4ec4744b0a
Ankit Kumar Gupta <itkan@users.noreply.github.com>
2014-06-16 12:16:18 +0530

    added comments

--- syntacticPlaygrounds/generics.playground/section-1.swift
+++ syntacticPlaygrounds/generics.playground/section-1.swift
@@ -2,10 +2,11 @@ import UIKit
 
 //generics
 
+//declaring a generic structure
 struct Stack<T> {
-    var elements = T[]()
+    var elements = T[]() //declaration of generic array
     
-    mutating func push(element:T) {
+    mutating func push(element:T) { //declaration of generic parameter
         elements.append(element)
     }
     
@@ -14,12 +15,13 @@ struct Stack<T> {
     }
 }
 
+//using generic class for Int
 var intStack = Stack<Int>()
 intStack.push(50)
 intStack
 intStack.pop()
 
-
+//using generic class for String 
 var stringStack = Stack<String>()
 stringStack.push("Hello")
 stringStack
